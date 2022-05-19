456196f2b9f40574105463bb69099db542ce5047
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-05-19 21:08:14 +0000

    Create product service
    - declare and initialize product model variable to an empty array.
    - declare an insertProduct function to handle the adding of products
    into product model variable.
    - return a json object to the controller after adding the product

---
+++ src/products/products.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { ProductModel } from './product.model';
+
+@Injectable()
+export class ProductsService {
+  productModel: ProductModel[] = [];
+
+  insertProduct(title: string, descirption: string, price: number) {
+    const prodId = Date.now().toString();
+    const newProduct = new ProductModel(prodId, title, descirption, price);
+
+    this.productModel.push(newProduct);
+    return { id: prodId };
+  }
+}
