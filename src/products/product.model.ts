b5834a784a2e73a9d078f6c30b4394eb8041621d
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-05-19 21:05:30 +0000

    Create product model

---
+++ src/products/product.model.ts
@@ -0,0 +1,20 @@
+export class ProductModel {
+  //   id: string;
+  //   title: string;
+  //   descirption: string;
+  //   price: number;
+
+  //   constructor(id: string, title: string, description: string, price: number) {
+  //     this.id = id;
+  //     this.title = title;
+  //     this.descirption = description;
+  //     this.price = price;
+  //   }
+
+  constructor(
+    private id: string,
+    private title: string,
+    private description: string,
+    private price: number,
+  ) {}
+}
