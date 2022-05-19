e435f88a98a4d6cd85b83fb5beacbe3035db2910
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-05-19 21:27:51 +0000

    Add function to return products
    - this function returns the items that have been added into the
      productModel variable as a json (i.e. object with an array of
    products)

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,14 +3,20 @@ import { ProductModel } from './product.model';
 
 @Injectable()
 export class ProductsService {
-  productModel: ProductModel[] = [];
+  private productModel: ProductModel[] = [];
 
-  // Add products Service
+  // Add products - service
   insertProduct(title: string, descirption: string, price: number) {
     const prodId = Date.now().toString();
     const newProduct = new ProductModel(prodId, title, descirption, price);
 
     this.productModel.push(newProduct);
     return { id: prodId };
+  }
+
+  //   Get products - service
+  getProducts() {
+    // return [...this.productModel]; we can do this to return a copy of the product model
+    return { products: this.productModel }; // this returns a pointer to the productModel memory location
   }
 }
