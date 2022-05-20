a99f056afa4bc263259244b1457fbaf5bd0a0151
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-05-20 12:38:31 +0000

    Add update and delete requests
    - create a patch decorator to handle product updates
    - create a delete decorator to handle product deletion

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -27,5 +35,25 @@ export class ProductsController {
       dto.description,
       dto.price,
     );
+  }
+
+  // Update products - controller
+  @Patch(':id')
+  updateProduct(
+    @Param('id') prodId: string,
+    @Body() dto: { title: string; description: string; price: number },
+  ) {
+    return this.productService.updateProduct(
+      prodId,
+      dto.title,
+      dto.description,
+      dto.price,
+    );
+  }
+
+  // Delete products - controller
+  @Delete(':id')
+  deleteProduct(@Param('id') prodId: string) {
+    return this.productService.deleteProduct(prodId);
   }
 }
