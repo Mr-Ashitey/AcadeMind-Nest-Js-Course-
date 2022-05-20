e61791b9a0452a4a3ba708054c7ab0fc2e22ff5a
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-05-20 11:55:11 +0000

    Add function to get single products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { ProductModel } from './product.model';
 
 @Injectable()
 export class ProductsService {
   private productModel: ProductModel[] = [];
+
+  //   Get all products - service
+  getAllProducts() {
+    // return [...this.productModel]; we can do this to return a copy of the product model
+    return { products: this.productModel }; // this returns a pointer to the productModel memory location
+  }
+
+  //   Get single product - service
+  getProduct(productId: string) {
+    const product = this.productModel.find((prod) => prod.id === productId);
+
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+    return { product };
+  }
 
   // Add products - service
   insertProduct(title: string, descirption: string, price: number) {
@@ -12,11 +28,5 @@ export class ProductsService {
 
     this.productModel.push(newProduct);
     return { id: prodId };
-  }
-
-  //   Get products - service
-  getProducts() {
-    // return [...this.productModel]; we can do this to return a copy of the product model
-    return { products: this.productModel }; // this returns a pointer to the productModel memory location
   }
 }
