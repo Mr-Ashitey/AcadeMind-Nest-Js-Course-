eda1e8de5f150f6787c59163a7f9b68289bfbd98
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-05-20 14:41:47 +0000

    Import mongoose module into app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://nestjs-cluster0:nestjs-cluster0@cluster0.irmov.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
