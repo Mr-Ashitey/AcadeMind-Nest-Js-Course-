ce665e2cf7a676aac9f7c5cf3d10b563ce2dd56f
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-05-20 14:42:27 +0000

    Create product schema and product interface to get access to schema

--- src/products/product.model.ts
+++ src/products/product.model.ts
@@ -1,20 +1,34 @@
-export class ProductModel {
-  //   id: string;
-  //   title: string;
-  //   descirption: string;
-  //   price: number;
+import * as mongoose from 'mongoose';
 
-  //   constructor(id: string, title: string, description: string, price: number) {
-  //     this.id = id;
-  //     this.title = title;
-  //     this.descirption = description;
-  //     this.price = price;
-  //   }
+export const ProductSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+});
 
-  constructor(
-    public id: string,
-    public title: string,
-    public description: string,
-    public price: number,
-  ) {}
+export interface Product {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
 }
+// export class ProductModel {
+//   //   id: string;
+//   //   title: string;
+//   //   descirption: string;
+//   //   price: number;
+
+//   //   constructor(id: string, title: string, description: string, price: number) {
+//   //     this.id = id;
+//   //     this.title = title;
+//   //     this.descirption = description;
+//   //     this.price = price;
+//   //   }
+
+//   constructor(
+//     public id: string,
+//     public title: string,
+//     public description: string,
+//     public price: number,
+//   ) {}
+// }
