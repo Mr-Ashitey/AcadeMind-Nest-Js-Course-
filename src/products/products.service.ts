745e0870bbd2d82faae445fecd8feaadfe2f1fd4
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-05-23 15:01:31 +0000

    Make all function asynchronous as well as wrap certain implementations in try catch

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { Product } from './product.model';
 
 @Injectable()
@@ -10,68 +15,80 @@ export class ProductsService {
   constructor(
     @InjectModel('Product') private readonly productModel: Model<Product>,
   ) {} //injects the product mongoose model
+
   //   Get all products - service
-  getAllProducts() {
-    // return [...this.productModel]; we can do this to return a copy of the product model
-    return { products: this.productModel }; // this returns a pointer to the productModel memory location
+  async getAllProducts() {
+    const products = await this.productModel.find();
+    return { products: products }; // this returns a pointer to the productModel memory location
   }
 
   //   Get single product - service
-  getProduct(productId: string) {
-    const product = this.findProduct(productId)[0];
+  async getProduct(productId: string) {
+    const product = await this.findProduct(productId);
 
     return { product };
   }
 
   // Add products - service
-  insertProduct(title: string, descirption: string, price: number) {
-    const prodId = Date.now().toString();
-    const newProduct = new this.productModel({ title, descirption, price });
+  async insertProduct(title: string, description: string, price: number) {
+    // const prodId = Date.now().toString();
+    const newProduct = new this.productModel({ title, description, price });
 
     // this.productModel.push(newProduct);
-    return { id: prodId };
+    const result = await newProduct.save();
+    return { id: result.id, msg: 'Product Added Successfully' };
   }
 
   //  Update products - service
-  updateProduct(
+  async updateProduct(
     productId: string,
     title: string,
     description: string,
     price: number,
   ) {
-    const [product, productIndex] = this.findProduct(productId);
+    try {
+      if (!title || !description || !price) {
+        throw new NotFoundException('All fields are required');
+      }
+      const updatedProduct = { title, description, price };
 
-    const updatedProduct = new ProductModel(
-      productId,
-      title ? title : product.title,
-      description ? description : product.description,
-      price ? price : product.price,
-    );
+      const result = await this.productModel.findOneAndUpdate(
+        { _id: productId },
+        updatedProduct,
+        { new: true },
+        // (err, doc) => {},
+      );
 
-    this.productModel[productIndex] = updatedProduct;
-
-    return { id: productId, msg: 'Product Updated Successfully' };
+      return { id: result.id, msg: 'Product Updated Successfully' };
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 
   // Delete products - service
-  deleteProduct(productId: string) {
-    const productIndex = this.findProduct(productId)[1];
-    this.productModel.splice(productIndex, 1);
+  async deleteProduct(productId: string) {
+    const result = await this.productModel.deleteOne({ _id: productId }).exec();
+
+    if (result.deletedCount === 0) {
+      throw new NotFoundException('Could not find the product');
+    }
 
     return { msg: 'Product Deleted Successfully' };
   }
 
   // Private method to find products
-  private findProduct(productId: string): [ProductModel, number] {
-    const product = this.productModel.find((prod) => prod.id === productId);
-    const productIndex = this.productModel.findIndex(
-      (prod) => prod.id === productId,
-    );
+  private async findProduct(productId: string): Promise<Product> {
+    // find product by id
+    try {
+      const product = await this.productModel.findById(productId).exec();
+      if (!product) {
+        throw new NotFoundException('Product not found');
+      }
 
-    if (!product || productIndex === -1) {
+      return product;
+    } catch (error) {
+      console.log(error.message);
       throw new NotFoundException('Product not found');
     }
-
-    return [product, productIndex];
   }
 }
